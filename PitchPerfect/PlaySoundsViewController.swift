432c4361b91e1eabcbf6adb66ad2af569e24af3e
Ansh Nanda <anshnanda10@gmail.com>
2018-06-18 20:43:52 +0530

    Initial commit (almost complete app)

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Ansh Nanda on 17/06/18.
+//  Copyright © 2018 Ansh Nanda. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton){
+        print("Play sound button pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject){
+        print("Stop audio button pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
